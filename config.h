ebfb0e09f42b926626bc432e024e92b22e3bc911
mathbike <mathbike8@gmail.com>
2022-03-12 03:59:00 -0500

    up

--- config.h
+++ config.h
@@ -1,23 +1,23 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int gappx     = 20;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrains Mono:size=14" }; /* top bar font */
+static const char *fonts[]          = { "JetBrains Mono:style=Bold:size=13" }; /* top bar font */
 static const char dmenufont[]       = "JetBrains Mono:style=Bold:size=10";
-static const char col_gray1[]       = "#222222";
+static const char col_gray1[]       = "#0c0d0e"; /*"#222222";*/
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_purple1[]     = "#993399"; /* color */
-static const char col_cyan[]        = "#005577";
+static const char col_purple1[]     = "#993399";
+static const char col_cyan[]        = "#31a354"; /*"#b7b8b9";*/
 static const char *colors[][3]      = {
 	/*               fg           bg         border   */
 	[SchemeNorm] = { col_purple1, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_purple1, col_gray1, col_purple1 },
+	[SchemeSel]  = { col_cyan, col_gray1, col_purple1 },
 };
 
 /* tagging */
